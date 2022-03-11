0fbfbab3599d34d6d832d6e72a1f4072b6f66f32
mayurmore14 <mayurmore8505@gmail.com>
2022-03-11 10:39:45 +0530

    ADDED: Line Comparison Program

---
+++ src/com/InfogalaxyInstitute2/LineComparisonProblem.java
@@ -0,0 +1,8 @@
+package com.InfogalaxyInstitute2;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch Developed by Mayur");
+
+    }
+}
